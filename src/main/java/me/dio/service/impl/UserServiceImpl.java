f82b17b9fc679f22026787a66849dc611fdbb848
Eryck Santos <dev.erycksantos@gmail.com>
2025-03-13 18:17:56 -0300

    Erro da camada Service consertado!

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package me.dio.service.impl;
 import me.dio.domain.model.User;
 import me.dio.domain.repository.UserRepository;
 import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
