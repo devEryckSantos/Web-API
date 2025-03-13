2d7888e84a1ea526cf789278cb4ddefacc512f3b
Eryck Santos <dev.erycksantos@gmail.com>
2025-03-13 12:17:33 -0300

    Hotfix scale e precision

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -18,9 +18,9 @@ public class Account {
 
     private String agency;
 
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, scale = 2, precision =13)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(name = "additional_limit", scale = 2, precision =13)
     private BigDecimal limit;
 }
